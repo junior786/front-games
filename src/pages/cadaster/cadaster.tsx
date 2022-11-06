04030cb8131f75571058eb77e72beaedac3b7eb4
Natacha Soares <soaresnatacha01@gmail.com>
2022-11-06 15:11:42 -0300

    pag. Cadastro

--- src/pages/cadaster/cadaster.tsx
+++ src/pages/cadaster/cadaster.tsx
@@ -1,5 +1,66 @@
+import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import "./styles.scss";
+import { Link } from "@mui/material";
+
+
 export  function Cadaster() {
     return (
-        <h1>CADASTER WORK</h1>
-    )
+        <section className="cadaster__form">
+      <form action="post" className="cadaster__action">
+        <TextField
+          className="input"
+          required
+          id="outlined-required"
+          label="Nome completo"
+          placeholder="Nome completo"
+        />
+        <TextField
+          className="input"
+          required
+          id="outlined-required"
+          label="Nome de usuário"
+          placeholder="Nome de usuário"
+        />
+        <TextField
+          className="input"
+          required
+          id="outlined-required"
+          label="E-mail"
+          placeholder="Email@gmail.com"
+        />
+        <TextField
+          className="input"
+          required
+          type="password"
+          id="outlined-required"
+          label="Senha"
+          placeholder="Senha"
+        />
+        <TextField
+          className="input"
+          required
+          type="password"
+          id="outlined-required"
+          label="Confirmar Senha"
+          placeholder="Confirmar Senha"
+        />
+        <div className="cadaster__password">
+          <FormGroup>
+            <FormControlLabel control={<Checkbox  />} label="Concordo com os" />
+          </FormGroup>
+          <Link href="#" underline="hover" rel="noopener"> {'Termos de uso'} </Link> 
+        </div>
+        <div className="cadaster__button">
+          <Button variant="outlined">Cadastrar</Button>
+        </div>
+      </form>
+      <div className="cadaster__line"></div>
+    </section>
+    
+    );
 }
+export default Cadaster;
